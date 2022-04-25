d7bf3b95098bc23a8ad34cb970c2e5c5641c02b1
yicen <a87835546@gmail.com>
2022-04-25 15:03:49 +0800

    Initial commit

---
+++ rocketmq-exmaple/src/main/java/com/example/rocketmqexmaple/RocketmqExmapleApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocketmqexmaple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqExmapleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqExmapleApplication.class, args);
+    }
+
+}
---
+++ rocketmq-exmaple/src/test/java/com/example/rocketmqexmaple/RocketmqExmapleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rocketmqexmaple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqExmapleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
